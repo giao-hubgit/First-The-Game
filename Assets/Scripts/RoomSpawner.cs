8bba75e0cd376aa9493b2075312d436f42b0aa2b
giao-hubgit <danggiao115@gmail.com>
2026-03-17 19:10:32 +0700

    Dungeon generation p2, fix bugs

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
@@ -9,46 +10,83 @@ public class RoomSpawner : MonoBehaviour
 
     private RoomTemplate template;
 
-    void Start()
+    void Awake()
     {
         template = GameObject.FindGameObjectWithTag("Room").GetComponent<RoomTemplate>();
-        Invoke("Spawn", 0.1f);
+    }
+
+    void Start()
+    {
+        // Mẹo nhỏ: Cộng thêm một chút thời gian ngẫu nhiên để tránh các spawner chạy cùng 1 frame
+        float randomDelay = 0.1f + UnityEngine.Random.Range(-0.02f, 0.02f);
+        Invoke("Spawn", randomDelay);
     }
 
     void Spawn()
     {
         if (spawned == false)
         {
-            if (openingDir == 1)
+            if (!spawned && template.currentRooms < template.maxRooms)
             {
-                rand = Random.Range(0, template.botRoom.Length);
-                Instantiate(template.botRoom[rand], transform.position, template.botRoom[rand].transform.rotation);
-            }
-            else if (openingDir == 2)
-            {
-                rand = Random.Range(0, template.topRoom.Length);
-                Instantiate(template.topRoom[rand], transform.position, template.topRoom[rand].transform.rotation);
-            }
-            else if (openingDir == 3)
-            {
-                rand = Random.Range(0, template.leftRoom.Length);
-                Instantiate(template.leftRoom[rand], transform.position, template.leftRoom[rand].transform.rotation);
-            }
-            else if (openingDir == 4)
-            {
-                rand = Random.Range(0, template.rightRoom.Length);
-                Instantiate(template.rightRoom[rand], transform.position, template.rightRoom[rand].transform.rotation);
-            }
-        }
+                if (openingDir == 1)
+                {
+                    rand = UnityEngine.Random.Range(0, template.botRoom.Length);
+                    Instantiate(template.botRoom[rand], transform.position, template.botRoom[rand].transform.rotation);
+                }
+                else if (openingDir == 2)
+                {
+                    rand = UnityEngine.Random.Range(0, template.topRoom.Length);
+                    Instantiate(template.topRoom[rand], transform.position, template.topRoom[rand].transform.rotation);
+                }
+                else if (openingDir == 3)
+                {
+                    rand = UnityEngine.Random.Range(0, template.leftRoom.Length);
+                    Instantiate(template.leftRoom[rand], transform.position, template.leftRoom[rand].transform.rotation);
+                }
+                else if (openingDir == 4)
+                {
+                    rand = UnityEngine.Random.Range(0, template.rightRoom.Length);
+                    Instantiate(template.rightRoom[rand], transform.position, template.rightRoom[rand].transform.rotation);
+                }
 
-        spawned = true;
+                template.currentRooms++;
+                spawned = true;
+            }
+            else if (!spawned && template.currentRooms >= template.maxRooms)
+            {
+                Instantiate(template.closedRoom, transform.position, Quaternion.identity);
+            }
+            spawned = true;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("SpawnPoint") && collision.GetComponent<RoomSpawner>().spawned == true)
+        // 1. Kiểm tra xem Object chạm vào có tag SpawnPoint không
+        if (collision.CompareTag("SpawnPoint"))
         {
-            Destroy(gameObject);
+            // 2. Lấy script RoomSpawner từ Object chạm vào
+            RoomSpawner otherSpawner = collision.GetComponent<RoomSpawner>();
+
+            // 3. Kiểm tra AN TOÀN: Đảm bảo script thực sự tồn tại (không bị Null)
+            if (otherSpawner != null)
+            {
+                // Nếu cả 2 đều chưa sinh phòng
+                if (otherSpawner.spawned == false && spawned == false)
+                {
+                    // Sinh ra phòng đóng (cụt) và tiêu hủy bản thân
+                    Instantiate(template.closedRoom, transform.position, Quaternion.identity);
+                    Destroy(gameObject);
+                }
+
+                // Đánh dấu là đã xử lý xong để không sinh thêm phòng đè lên
+                spawned = true;
+            }
+            else
+            {
+                // Cảnh báo trên Console nếu bạn vô tình gắn nhầm tag
+                Debug.LogWarning("Có một Object mang tag 'SpawnPoint' nhưng không có script RoomSpawner: " + collision.gameObject.name);
+            }
         }
     }
 }
--- Assets/Scripts/RoomTemplate.cs
+++ Assets/Scripts/RoomTemplate.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class RoomTemplate : MonoBehaviour
 {
+    public int maxRooms;
+    public int currentRooms = 0;
+
     public GameObject[] botRoom;
     public GameObject[] topRoom;
     public GameObject[] leftRoom;
     public GameObject[] rightRoom;
+
+    public GameObject closedRoom;
+
+    void Awake()
+    {
+        maxRooms = Random.Range(10, 15);
+    }
 }
